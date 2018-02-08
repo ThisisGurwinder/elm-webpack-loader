09319a2415f81cf2dcf663cb0bf3ab4c6099d23b
Gurwinder <ThisisGurwinder@Gmail.com>
2018-02-08 18:51:08 +0530

    Update Main.elm

--- example-wp2/src/Main.elm
+++ example-wp2/src/Main.elm
@@ -27,5 +27,31 @@ view username password =
         [ CDN.stylesheet
         , node "link" [ rel "stylesheet", href "http://v4-alpha.getbootstrap.com/examples/signin/signin.css" ] []
         , Form.form [ class "form-signin" ]
-            [ h2 [ class "form-signin-heading" ] [ text "Please sign in " ]
-            , label [ class "sr-only"] ]]
+            [ h2 [ class "form-signin-heading" ] [ text "Please sign in" ]
+            , label [ class "sr-only", for "inputEmail" ] [ text "Email address" ]
+            , Input.email
+                [ Input.id "inputEmail"
+                , Input.placeholder "Email address"
+                , Input.attrs [ required True, autofocus True ]
+                , Input.onInput EmailUpdated
+                , Input.value username
+                ]
+            , label [ class "sr-only", for "inputPassword" ] [ text "Password" ]
+            , Input.password
+                [ Input.id "inputPassword"
+                , Input.placeholder "Password"
+                , Input.attrs [ required True ]
+                , Input.onInput PasswordUpdated
+                , Input.value password
+                ]
+            , Checkbox.checkbox [ Checkbox.onCheck RememberMeToggled ]
+                "Remember me"
+            , Button.button
+                [ Button.large
+                , Button.block
+                , Button.primary
+                , Button.attrs [ type_ "submit", onSubmit SigninPressed ]
+                ]
+                [ text "Sign in" ]
+            ]
+        ]
